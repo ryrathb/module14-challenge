e95902d35eb883d60fc3ad9ba704b494be9eb72c
Ryan Rathbun <ryrathb@arizona.edu>
2022-08-30 21:01:44 -0700

    push

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/views/layout/main.handlebars'))
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
