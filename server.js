3bdee5b09dfdc23b1057c94df50dae25b2f23e19
Ryan Rathbun <ryrathb@arizona.edu>
2022-09-13 18:08:30 -0700

    push

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/views/layout/main.handlebars'))
+  res.sendFile(path.join(__dirname, '/views/layouts/main.handlebars'))
 );
 
 app.listen(PORT, () => {
