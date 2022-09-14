34bdd65ee8bd6fd46d408ef39afedf3ffbef60b4
Ryan Rathbun <ryrathb@arizona.edu>
2022-09-13 17:48:21 -0700

    push

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,3 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./home-routes');
-
-router.use('/', homeRoutes);
-
 module.exports = router;
--- server.js
+++ server.js
@@ -6,19 +6,14 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection.js");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const sess = {
     secret: "secret",
     cookie: {},
     resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+    saveUninitialized: true
+}
 
 app.use(session(sess));
 
